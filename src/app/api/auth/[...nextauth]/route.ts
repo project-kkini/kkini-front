4f810c9400b1df9c1c03bbfc09806a933da7a9d2
Doeunnkimm <doeunnkimm@gmail.com>
2025-01-21 23:33:12 +0900

    feat: next-auth 로그인 callback 처리 기본 구성

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,34 @@
+import { http } from '@/apis/http';
 import NextAuth from 'next-auth';
-import { NextAuthOptions } from 'next-auth';
+import { NextAuthOptions, CallbacksOptions } from 'next-auth';
 import kakaoProvider from 'next-auth/providers/kakao';
+
+/**
+ * @returns 로그인에 성공한 경우 true, 실패한 경우 false를 반환합니다.
+ */
+const socialLogin: CallbacksOptions['signIn'] = async ({ account }) => {
+  const accessToken = account?.access_token;
+  const provider = account?.provider;
+
+  if (accessToken == null || provider == null) return false;
+
+  try {
+    const { data } = await http.post(
+      `/auth/oauth/social-login?provider=${provider}`,
+      {},
+      { headers: { social_access_token: accessToken } },
+    );
+    // @TODO 로그인 성공 시 처리하기
+    return true;
+  } catch (error) {
+    /**
+     * @TODO
+     * 로그인 실패 시 처리하기. 지금은 next-auth에서 지정해놓은 에러 리다이렉트 페이지로 가고 있음.
+     */
+    console.error(error);
+    return false;
+  }
+};
 
 const authOptions: NextAuthOptions = {
   providers: [
@@ -10,6 +38,9 @@ const authOptions: NextAuthOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    signIn: socialLogin,
+  },
 };
 
 const handler = NextAuth(authOptions);
