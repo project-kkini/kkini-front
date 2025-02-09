22b35417bbab05bff9c5ac158804a2252640d5e3
Doeunnkimm <doeunnkimm@gmail.com>
2025-02-09 20:43:32 +0900

    chore: 주석 추가

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -8,7 +8,15 @@ import { BottomSheetTitle, BottomSheetTitleProps } from './compound/Title';
 import { BottomSheetTrigger, BottomSheetTriggerProps } from './compound/Trigger';
 
 type BottomSheetRootProps = RootProps;
-function BottomSheet({ autoFocus = true, ...props }: BottomSheetRootProps) {
+function BottomSheet({
+  /**
+   * open 상태가 되었을 때, Overlay에 `aria-hidden="true"`가 되지만 focus가 남아있어 에러가 발생합니다.
+   * open 상태가 되었을 때 자동으로 focus를 받을 수 있도록 autoFocus를 기본적으로 true로 설정합니다.
+   * https://github.com/emilkowalski/vaul/issues/517#issuecomment-2571619213
+   */
+  autoFocus = true,
+  ...props
+}: BottomSheetRootProps) {
   return <Drawer.Root autoFocus={autoFocus} {...props} />;
 }
 const BottomSheetClose = Drawer.Close;
