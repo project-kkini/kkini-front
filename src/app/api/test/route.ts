b831b3716d9b41191caef25442c3e444dc046677
Doeunnkimm <doeunnkimm@gmail.com>
2025-03-03 16:15:41 +0900

    rm

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,7 +3,6 @@ import { http } from '@/apis/http';
 import { NextResponse } from 'next/server';
 
 export const GET = async () => {
-  console.log('호출?');
   try {
     const { data } = await http.get('/v1/restaurants/1');
     return NextResponse.json(data);
