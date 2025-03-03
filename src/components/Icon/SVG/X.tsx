05bfe3a41786e46b60b4d84e9874a7bbbdc9a005
Doeunnkimm <doeunnkimm@gmail.com>
2025-03-03 22:01:15 +0900

    fix: viewBox

--- src/components/Icon/SVG/X.tsx
+++ src/components/Icon/SVG/X.tsx
@@ -2,7 +2,7 @@ import { IconBaseProps } from './types';
 
 export const X = ({ size, color }: IconBaseProps) => {
   return (
-    <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`} fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width={size} height={size} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M15 5L5 15" stroke={color} stroke-linecap="round" stroke-linejoin="round" />
       <path d="M15 15L5 5" stroke={color} stroke-linecap="round" stroke-linejoin="round" />
     </svg>
